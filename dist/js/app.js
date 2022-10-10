9c3395f3f9d3dca6eeac0be3c61acd8aec829140
shmelevsergei <shmelevsergei86@gmail.com>
2022-10-11 01:14:38 +0300

    50% to end

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,9 +1,31 @@
-const swiper = new Swiper('.slider-photo', {
+const swiperPhoto = new Swiper('.slider-photo', {
   navigation: {
     nextEl: '.button-next',
     prevEl: '.button-prev',
   },
   slidesPerView: 2,
   spaceBetween: 30,
+  loop: true,
+});
 
+const swiperNumber = new Swiper('.slider-number', {
+  navigation: {
+    nextEl: '.number-slider__next',
+    prevEl: '.number-slider__prev',
+  },
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  loop: true,
+});
+
+const swiperNumberFooter = new Swiper('.footer-number-slider', {
+  navigation: {
+    nextEl: '.footer-number-button-next',
+    prevEl: '.footer-number-button-prev',
+  },
+  slidesPerView: 4,
+  spaceBetween: 30,
+  loop: true,
 });
